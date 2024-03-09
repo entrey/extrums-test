6b0022efe2bab4f2a5b00ab5e8b1b35b88192ef3
Roman Peniaz <peniaz.roman@gmail.com>
2024-03-09 16:43:31 +0200

    form markup

--- wp-content/plugins/extrums-test/admin/partials/extrums-test-admin-display.php
+++ wp-content/plugins/extrums-test/admin/partials/extrums-test-admin-display.php
@@ -6,6 +6,82 @@
  * @link    https://github.com/entrey
  * @since   1.0.0
  */
-?>
 
-<!-- This file should primarily consist of HTML with a little bit of PHP. -->
+
+$the_replace_form = function ( $column_name ) {
+    ?>
+    <form class="replace-form">
+        <input
+            type="text"
+            id="<?php echo esc_attr( "post-new-$column_name" ); ?>"
+            placeholder="<?php esc_attr_e( 'new keyword...', 'extrums-test' ); ?>"
+        >
+        <input
+            type="submit"
+            value="<?php esc_attr_e( 'Replace', 'extrums-test' ); ?>"
+        >
+    </form>
+    <?php
+};
+
+?>
+<div class="extrums">
+    <form class="query-form">
+        <input
+            type="text"
+            id="post-keyword"
+            placeholder="<?php esc_attr_e( 'keyword...', 'extrums-test' ); ?>"
+        >
+        <input
+            type="submit"
+            value="<?php esc_attr_e( 'Search', 'extrums-test' ); ?>"
+        >
+    </form>
+
+    <div class="query-result">
+        <h2 class="title">
+            <?php esc_html_e( 'Results for', 'extrums-test' ); ?>
+            <span class="keyword"></span>
+        </h2>
+
+        <table class="result__table">
+            <tbody>
+                <tr class="table__row">
+                    <th class="table__column title">
+                        <h3>
+                            <?php esc_html_e( 'Title', 'extrums-test' ); ?>
+                        </h3>
+                        <?php $the_replace_form( 'title' ); ?>
+                    </th>
+                    <th class="table__column content">
+                        <h3>
+                            <?php esc_html_e( 'Content', 'extrums-test' ); ?>
+                        </h3>
+                        <?php $the_replace_form( 'content' ); ?>
+                    </th>
+                    <th class="table__column meta-title">
+                        <h3>
+                            <?php esc_html_e( 'Meta-title', 'extrums-test' ); ?>
+                        </h3>
+                        <?php $the_replace_form( 'meta-title' ); ?>
+                    </th>
+                    <th class="table__column meta-description">
+                        <h3>
+                            <?php esc_html_e( 'Meta-description', 'extrums-test' ); ?>
+                        </h3>
+                        <?php $the_replace_form( 'meta-description' ); ?>
+                    </th>
+                </tr>
+            </tbody>
+        </div>
+    </div>
+</div>
+
+<script type="text/template" id="extrums-tmpl-table-row">
+    <tr>
+        <td class="table__column title"></td>
+        <td class="table__column content"></td>
+        <td class="table__column meta-title"></td>
+        <td class="table__column meta-description"></td>
+    </tr>
+</script>
